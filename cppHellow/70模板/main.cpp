5efd7254e8f9ec84f3373c55178cd5059d5f0313
wutong <tong.wu@netviewtech.com>
2020-10-05 12:34:10 +0800

    简单到类模板

---
+++ cppHellow/70模板/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  70模板
+//
+//  Created by codew on 10/5/20.
+//  Copyright © 2020 codertom. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+void mySwap(T a, T b)
+{
+    
+}
+
+template <typename T>
+void func2(T a, T b)
+{
+}
+
+
+int main()
+{
+    cout << "hello model" << endl;
+    
+    return 0;
+}
+
