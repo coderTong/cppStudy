850a3732c58a1296b63e5c716ce5187d566f41a0
wutong <tong.wu@netviewtech.com>
2020-10-22 21:57:53 +0800

    vector 容器, deque容器

---
+++ cppHellow/111打分案例/main.cpp
@@ -0,0 +1,127 @@
+//
+//  main.cpp
+//  111打分案例
+//
+//  Created by codew on 10/22/20.
+//  Copyright © 2020 codertom. All rights reserved.
+//
+
+#include <iostream>
+
+#include <vector>
+#include <deque>
+#include <string>
+#include <algorithm> // 算法头文件
+#include <ctime>
+
+using namespace std;
+
+// 目的: 5个学生, 10个评委, 10个评委的分数去掉 最高和最低分, 取平均分就是分数的分数
+// 抽象学生
+class Student
+{
+public:
+    string name;
+    int mScore;
+};
+
+// 1. 创建学生
+void createStudent(vector<Student> &vstu)
+{
+    string setName = "ABCDE";
+    
+    for (int i = 0; i < 5; i++) {
+        
+        // 创建学生
+        Student stu;
+        stu.name = "学生";
+        stu.name += setName[i];
+        stu.mScore = 0;
+        vstu.push_back(stu); // 把学生 放入容器
+        
+    }
+    
+}
+
+// 2. 评委给 学生 打分
+void setScore( vector<Student> &vstu)
+{
+    srand( (unsigned int)time(NULL) );
+    
+    // 遍历学生
+    for ( vector<Student>::iterator it = vstu.begin(); it != vstu.end(); it++) {
+        
+        // 保存分数
+        deque<int> dScore;
+        // 评委给学生打分
+        for ( int i = 0; i < 10; i++) {
+            
+            int score = rand() % 70 + 30;
+            dScore.push_back(score);
+        }
+        
+        // 排序
+        sort(dScore.begin(), dScore.end());
+        
+        
+        // 去掉最高分
+        dScore.pop_back();
+        dScore.pop_front();
+        
+        // 求总分
+        int tota = 0;
+        for (deque<int>::iterator sit = dScore.begin(); sit != dScore.end(); ++sit) {
+            
+            tota += (*sit);
+        }
+        
+        // 求平均分
+        int agescore = tota / dScore.size();
+        
+        // 平均分 存储到 对象中
+        it->mScore = agescore;
+    }
+    
+
+}
+
+
+bool myComapre(Student &s1, Student &s2)
+{
+    return s1.mScore > s2.mScore;
+}
+
+// 3. 排名并打印
+void showStudentScore( vector<Student> &vstu)
+{
+    sort(vstu.begin(), vstu.end(), myComapre);
+    
+    for (vector<Student>::iterator it = vstu.begin(); it != vstu.end(); ++it) {
+        
+        cout << "Name:" << it->name << " Score:" << it->mScore << endl;
+    }
+}
+
+void test()
+{
+    // 存储学生的容器
+    vector<Student> vstu;
+    
+    // 1. 创建学生
+    createStudent(vstu);
+    
+    // 2. 评委给学生打分
+    setScore(vstu);
+    
+    // 3. 排名并打印
+    showStudentScore(vstu);
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    test();
+    
+    return 0;
+}
---
+++ cppHellow/112stack容器/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  112stack容器
+//
+//  Created by codew on 10/22/20.
+//  Copyright © 2020 codertom. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
